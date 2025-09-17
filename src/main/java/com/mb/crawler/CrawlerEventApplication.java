aed464d5fc8fbeefdc795dccecd2aa7125a7224b
marcelbostan <bostanmarcel@gmail.com>
2025-09-17 22:24:53 +0300

    Init

---
+++ src/main/java/com/mb/crawler/CrawlerEventApplication.java
@@ -0,0 +1,13 @@
+package com.mb.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mb/crawler/CrawlerEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mb.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
